c0d59ca5a552eadf962d26c94e69c46788eb7423
Philip Schl <schloesph@web.de>
2013-11-15 22:41:15 +0100

    Updated Server.

--- src/de/philip/net/server/Server.java
+++ src/de/philip/net/server/Server.java
@@ -1,19 +1,31 @@
 package de.philip.net.server;
 
+import java.net.InetAddress;
+import java.net.Socket;
+
 import de.philip.util.Logger;
 
 public class Server {
-	
+
+	Socket sock;
+
 	public static final int DEFAULT_PORT = 33576;
-	
+
 	private int port;
-	
+
 	public Server(int port) {
 		this.port = port;
+		try {
+			sock = new Socket(InetAddress.getLocalHost(), port);
+		} catch (Exception e) {
+			Logger.err(e.getMessage());
+		}
 	}
-	
+
 	public void start() {
 		Logger.log("Starting Server on port " + port + " ...");
+		
+		
 	}
 
 	public int getPort() {
