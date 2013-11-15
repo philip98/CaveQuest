699d8de6b552a9ab72e2271ac6f8030f2018e689
Philip Schl <schloesph@web.de>
2013-11-15 19:48:03 +0100

    MenuRenderEngine now draws a button for Quit which does not respond to clicks.

--- src/de/philip/graphics/MenuRenderEngine.java
+++ src/de/philip/graphics/MenuRenderEngine.java
@@ -1,6 +1,7 @@
 package de.philip.graphics;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import de.philip.CaveQuest;
@@ -12,9 +13,17 @@ public class MenuRenderEngine extends RenderEngine {
 	}
 	
 	public void render() {
+		int h = CaveQuest.height;
+		int w = CaveQuest.width;
 		Graphics g = super.getGraphics();
-		g.setColor(Color.cyan);
-		g.fillRect(0, 0, CaveQuest.getInstance().getWidth(), CaveQuest.getInstance().getHeight());
+		
+		g.setColor(Color.decode("0xCCCCFF"));
+		g.fillRect(0, 0, w, h);
+		g.setColor(Color.decode("0x8888FF"));
+		g.fillRect(100, 300, 700, 100);
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Roman", Font.BOLD, 48));
+		g.drawString("Quit", 180, 362);
 	}
 
 }
